9ec95867e076c7e60e8d583629e35d37d587919f
mrsk7 <mirozis@hotmail.com>
2015-05-20 15:14:50 +0300

    finished

--- happy.c
+++ happy.c
@@ -2,13 +2,6 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-void logEntry(const char *func) {
-	printf("Entry: %s\n",func);
-}	
-
-void logExit(const char *func) {
-	printf("Exit: %s\n",func);
-}	
 
 int ipow(int base, int exp) {
     int result = 1;
@@ -21,7 +14,6 @@ int ipow(int base, int exp) {
 }
 
 int getNumOfDigits(int x) {
-	int i=10;
 	int ret=0;
 	while (x>=1) {
 		x/=10;
@@ -32,7 +24,7 @@ int getNumOfDigits(int x) {
 
 bool isHappy(int x) {
 	if (x==1) return true;
-	else if (x==4) return false;
+	else if (x==4 || x==0) return false;
 	else {
 		if (x<10) return isHappy(ipow(x,2));
 		else if (x<100) return isHappy(ipow(x/10,2)+ipow(x%10,2));
@@ -40,74 +32,86 @@ bool isHappy(int x) {
 	}
 }
 
-computeAllHappy(bool *array, int size) {
+void computeAllHappy(int *array, int size) {
 	int i;
 	for (i=1;i<=size;i++) {
-		if (isHappy(i)) array[i] = true;
-		else array[i] = false;
+		if (isHappy(i)) array[i] = 1;
+		else array[i] = 0;
 	}
 }
 
-
-void countHappyNumbers(int **p,bool *happy,int N) {
-    int exp = getNumOfDigits(N);
-    printf("exp is %d\n",exp);
-    int i,j,offset;
-    //Init first row, thus set p[0][1],p[0][4],p[0][9],p[0][16]...p[0][81] to 1
-    for (i=1;i<=9;i++) p[0][i*i] = 1;
-    for (i=1;i<=9;i++) printf("%d\n",p[0][i*i]);
-    
-    for (i=1;i<=9;i++) {
-        printf("i is %d\n",i);
-        printf("j limit is %d\n",(i-1)*81);
-        for (j=0;j<=(i-1)*81;j++) {
-            printf("  j is %d\n",j);
-            for (offset=0;offset<10;offset++) {
-                printf("previous: p[%d][%d]=%d, after :p[%d][%d]=%d\n",i-1,j,p[i-1][j],i,j+offset*offset,p[i][j+offset*offset]);
-                p[i][j+offset*offset]+=p[i-1][j];
-            }
-        }
+int countUpToLimit(int **p,int lim,int *happy) {
+    if (lim==0) return 0;
+    int digits=0;
+    int row = getNumOfDigits(lim)-1; //for example if XYZ is given as limit, I will search at 2nd row([0,2*81])
+    int i,j,cnt,upper;
+    cnt=0;
+    int divider=ipow(10,row);   //for XYZ divider is starting as 100 so that we get X first
+    while (divider>1) {
+        upper = (lim/divider)%10;
+        for (i=0;i<upper;i++) 
+            for (j=0;j<=row*81;j++) 
+                    cnt+= p[row][j]*happy[j+i*i+digits];
+        divider/=10;
+        digits+=upper*upper;
+        row--;
     }
+    upper = (lim/divider)%10;
+    for (i=0;i<=upper;i++) 
+        if (isHappy(digits+i*i)) cnt++;
+    return cnt;
 }
-    
+
+
+void fillArray(int **p,int lim) {
+	int n = getNumOfDigits(lim);
+        int i,j,offset;
+	for (i=0;i<10;i++) p[1][i*i]=1;
+	for (i=2;i<=n;i++) { 
+            for (j=0;j<=(i-1)*81;j++) 
+                for (offset=0;offset<10;offset++) 
+                    p[i][j+offset*offset]+=p[i-1][j];
+        }
+} 
+  
+
+
 int main (int argc, char *argv[]) {
-    char *input = argv[1] ;
-	FILE *file;
-    if (argc<2) {
-        printf("Wrong execution. Try %s input_file\n",argv[0]);
-        exit(1);
-    }
-    file = fopen(input,"r");
-	int A,B,dummy,i;
-    if ((dummy = fscanf(file,"%d%d",&A,&B)) == 0) {
+        char *input = argv[1] ;
+            FILE *file;
+        if (argc<2) {
+            printf("Wrong execution. Try %s input_file\n",argv[0]);
+            exit(1);
+        }
+        file = fopen(input,"r");
+	int A,B,dummy,i,cnt;
+        if ((dummy = fscanf(file,"%d%d",&A,&B)) == 0) {
 		printf("Error reading A,B\n");
 		exit(1);
 	}
-	int pwr=getNumOfDigits(B);
-	int max = pwr*81;			//Maximum sum of squares that can be found is pwr*81 where pwr is the number of digits of digits
-	bool *happy = (bool *) malloc(max*sizeof(bool)+1);	//Allocation of array with size max
+	int *happy = (int *) malloc(9*81*sizeof(int)+1);	//Allocation of array with size max
 	if (happy==NULL) {
 		printf("Error allocating memory(happy)\n");
 		exit(1);
 	}
-	computeAllHappy(happy,max);		//Compute all happy numbers up to max. Maximum for max is 732=9*81
-	pwr=getNumOfDigits(A);
-        max=pwr*81;
-        int **array = malloc(pwr*sizeof(int *)+1);	//Allocation of array with size A
+        computeAllHappy(happy,9*81);
+	int **array = malloc(10*sizeof(int *));	//Allocation of array with size A
 	if (array==NULL) {
 		printf("Error allocating memory(array)\n");
 		exit(1);
 	}
-        for (i=0;i<pwr+1;i++) {
-            array[i] = malloc(max*sizeof(int)+1);
+        for (i=0;i<10;i++) {
+            array[i] = malloc(9*81*sizeof(int));
             if (array[i]==NULL) {
                     printf("Error allocating memory(array[i])\n");
                     exit(1);
             }
         }
-        printf("Successfully allocated array[%d][%d]\n",pwr+1,max+1);
-        printf("A is %d\n",A);
-        countHappyNumbers(array,happy,A);
+        fillArray(array,B);
+	cnt = countUpToLimit(array,B,happy);
+        cnt-= countUpToLimit(array,A-1,happy);
+        if (B==ipow(10,9)) cnt++;      //Because 10^9 is happy but array is up to 9*81, thus 10^9-1 we just increment cnt
+	printf("%d\n",cnt);
         free(array);
 	free(happy);
 	fclose(file);
